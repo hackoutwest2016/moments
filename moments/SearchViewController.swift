b7807df1aa745900502d21faa12a1e3ed86f6ba0
Pak <radishlau@gmail.com>
2016-08-09 20:54:39 +0200

    Added song name and artists

--- moments/SearchViewController.swift
+++ moments/SearchViewController.swift
@@ -12,33 +12,92 @@
 import Mapbox
 
 
-class SearchViewController: UIViewController, MGLMapViewDelegate {
+class SearchViewController: UIViewController, MGLMapViewDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    
     
     var userCoordinate:CLLocationCoordinate2D? = CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06)
     //it should be ?
     
+    typealias song = (artist:String,song:String)
+    var dataSource:[song] = []
+    let textCellIdentifier = "TrackCell"
+    
+    @IBOutlet weak var songListView: UITableView!
     @IBOutlet weak var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpMiniMap()
+        
+        setUpSongListView()
         searchSong()
-     
+        
+        
     }
+    
+    func setUpSongListView(){
+        songListView.delegate = self
+        songListView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = dataSource[row].song
+        cell.detailTextLabel?.text = dataSource[row].artist
+        
+        return cell
+    }
+    
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let row = indexPath.row
+        //print(swiftBlogs[row])
+    }
+    
     
     func searchSong(){
         
         let search = SPTSearch()
         
         SPTRequest.performSearchWithQuery("ta det lugnt", queryType: SPTSearchQueryType.QueryTypeTrack, session: nil) { (error, data) in
-            print(data)
+            //print(data)
             let items = (data as! SPTListPage).items
-            print(items)
+            //print(items)
             
             //take the first one
             let selectedSong = items.first as! SPTPartialTrack
-            print(selectedSong.name)
-            print(selectedSong.artists)
+//            print(selectedSong.name)
+//            print(selectedSong.artists)
             
+            //update the table
+            for n in 0..<items.count {
+                let currentItem = items[n] as! SPTPartialTrack
+                print(currentItem.artists)
+                
+                //just gonna pretend there is only one artist
+                let artist = currentItem.artists.first as! SPTPartialArtist
+                let songName = currentItem.name
+                let currentSong:song = (artist.name , songName)
+                self.dataSource.append(currentSong)
+            }
+           
+            
+          
+            
+            self.songListView.reloadData()
         }
         
         
