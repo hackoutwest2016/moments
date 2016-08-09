b96d6b48438ae915f080d5cae2f54e997d1ca1bf
Pak <radishlau@gmail.com>
2016-08-09 12:02:28 +0200

    Finished the spotify login screen. Contains a popup which we might need to fix later.

--- moments/LoginViewController.swift
+++ moments/LoginViewController.swift
@@ -6,30 +6,100 @@
 //  Copyright © 2016 paksnicefriends. All rights reserved.
 //
 
-import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, SPTAuthViewDelegate, SPTAudioStreamingDelegate {
+    var player:SPTAudioStreamingController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(animated: Bool) {
+        let auth = SPTAuth.defaultInstance()
+        auth.clientID        = "f51e1e2a1bad4bbfaa3350f7472696d7"
+        auth.redirectURL     = NSURL.init(string:"moments-login://callback")
+        auth.requestedScopes = [SPTAuthStreamingScope]
+        
+        let authvc = SPTAuthViewController.authenticationViewController()
+        // authvc.clearCookies(nil)
+        authvc.modalPresentationStyle   = UIModalPresentationStyle.OverCurrentContext
+        authvc.modalTransitionStyle     = UIModalTransitionStyle.CrossDissolve
+        authvc.delegate                 = self
+        
+        self.modalPresentationStyle     = UIModalPresentationStyle.CurrentContext
+        self.definesPresentationContext = true
+        self.presentViewController(authvc, animated: true, completion: nil)
     }
-    */
-
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
+        print("Login")
+        self.loginUsingSession(session)
+        
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didFailToLogin error: NSError!) {
+        print("Fail to login")
+    }
+    
+    func authenticationViewControllerDidCancelLogin(authenticationViewController: SPTAuthViewController!) {
+        print("Cancel login")
+    }
+    
+    
+    
+    
+    func loginUsingSession(session:SPTSession){
+        // Get the player instance
+        self.player = SPTAudioStreamingController.sharedInstance()
+        self.player!.delegate = self;
+        // Start the player (will start a thread)
+        do {
+            try self.player?.startWithClientId("e68a7d2684a1480a92c76af243bf0a30")
+            print("try player")
+        }
+        catch _{
+            print("catch error in loginUS")
+        }
+        
+        // self.player startWithClientId:@"e68a7d2684a1480a92c76af243bf0a30" error:nil];
+        // Login SDK before we can start playback
+        self.player?.loginWithAccessToken(session.accessToken)
+        
+    }
+    
+    func audioStreamingDidLogin(audioStreaming: SPTAudioStreamingController!) {
+        
+        let option = SPTPlayOptions()
+        option.startTime = 50
+        
+        let trackUri:NSURL! = NSURL.init(string: "spotify:track:5WBxAaS9nNnWTH469p0Ht0")
+        // self.player!.playURIs([trackUri], fromIndex: 0,  callback: nil)
+        
+        
+        
+        self.player!.playURIs([trackUri], withOptions: option, callback: nil)
+        
+        
+        //        let option = SPTPlayOptions()
+        //        option.startTime = 0.4
+        //
+        //
+        //        let firstSong = NSURL.init(string: "spotify:track:5WBxAaS9nNnWTH469p0Ht0")
+        //        var trackUris = [NSURL]()
+        //        trackUris.append(firstSong!)
+        //
+        //
+        //        self.player!.playURIs(trackUris, withOptions: option) { (err) in
+        //            print(err!)
+        //        }
+        //        
+        
+    }
+    
 }
