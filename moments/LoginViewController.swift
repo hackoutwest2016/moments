12a5abf3109a2bc5430bfd87792a8bc270f2e5ff
Simon Nilsson <sim.nilsson@gmail.com>
2016-08-11 10:54:24 +0200

    Intro animation

--- moments/LoginViewController.swift
+++ moments/LoginViewController.swift
@@ -9,14 +9,30 @@
 
 class LoginViewController: UIViewController, SPTAuthViewDelegate {
     
+    @IBOutlet weak var animationImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var animationImages = [UIImage]()
+        
+        for i in 0...84 {
+            let animationFrame = UIImage(named: "moments\(i)")
+            animationImages.append(animationFrame!)
+        }
+        
+        animationImageView.animationImages = animationImages
+        animationImageView.animationDuration = 2
+        animationImageView.animationRepeatCount = 1
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        animationImageView.startAnimating()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -28,20 +44,23 @@ class LoginViewController: UIViewController, SPTAuthViewDelegate {
         tag["videos"] = [video1,video2]
         tag.saveInBackground()
         */
-        let auth = SPTAuth.defaultInstance()
-        auth.clientID        = MomentsConfig.spotify.clientId
-        auth.redirectURL     = NSURL.init(string:MomentsConfig.spotify.redirectUrl)
-        auth.requestedScopes = [SPTAuthStreamingScope]
         
-        let authvc = SPTAuthViewController.authenticationViewController()
-        // authvc.clearCookies(nil)
-        authvc.modalPresentationStyle   = UIModalPresentationStyle.OverCurrentContext
-        authvc.modalTransitionStyle     = UIModalTransitionStyle.CrossDissolve
-        authvc.delegate                 = self
-        
-        self.modalPresentationStyle     = UIModalPresentationStyle.CurrentContext
-        self.definesPresentationContext = true
-        self.presentViewController(authvc, animated: true, completion: nil)
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (Int64)(3 * NSEC_PER_SEC)), dispatch_get_main_queue()) {
+            let auth = SPTAuth.defaultInstance()
+            auth.clientID        = MomentsConfig.spotify.clientId
+            auth.redirectURL     = NSURL.init(string:MomentsConfig.spotify.redirectUrl)
+            auth.requestedScopes = [SPTAuthStreamingScope]
+            
+            let authvc = SPTAuthViewController.authenticationViewController()
+            // authvc.clearCookies(nil)
+            authvc.modalPresentationStyle   = UIModalPresentationStyle.OverCurrentContext
+            authvc.modalTransitionStyle     = UIModalTransitionStyle.CrossDissolve
+            authvc.delegate                 = self
+            
+            self.modalPresentationStyle     = UIModalPresentationStyle.CurrentContext
+            self.definesPresentationContext = true
+            self.presentViewController(authvc, animated: true, completion: nil)
+        }
     }
     
     func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
